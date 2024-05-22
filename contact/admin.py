35e4c9d737a5489d1b59652dc406d270b5717455
Raphael <raphaelrodc@gmail.com>
2024-05-22 18:12:51 -0300

    Atualizacoes de conf

--- contact/admin.py
+++ contact/admin.py
@@ -9,5 +9,10 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'last_name',
-    list_display_links = 'id', 'first_name',
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
